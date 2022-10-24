1a1f72e5a10f277d855de0083e8cb9b8bb714685
ckafff <borisnovoselov602@gmail.com>
2022-10-24 07:53:13 +0300

    tests

--- src/Solution1.hs
+++ src/Solution1.hs
@@ -3,44 +3,53 @@ import Data.Set as Set
 import Data.List (sort)
 
 -- хвостовая рекурсия
-
+p1_a :: Int -> Int
 p1_a n | n == 0 = 0
        | n `mod` 5 == 0 || n `mod` 3 == 0 = n + p1_a(n-1)
        | otherwise = p1_a (n-1)
 
+p1 :: Int
 p1 = p1_a 1000
 -- рекурсия
-p5_a :: Integer -> Integer
-p5_a n = if n <= 1000
-    then
-      if  (n `mod` 5 == 0 )||( n `mod` 3 == 0)
-      then n + p5_a(n+1)
-      else p5_a(n+1)
-    else 0
+p5_a :: Int -> Int
+p5_a n | (n <= 1000 && ((n `mod` 5 == 0 )||( n `mod` 3 == 0))) = n + p5_a(n+1)
+       | n <= 1000 = p5_a(n+1)
+       | otherwise = 0
 
+p5 :: Int
 p5 = p5_a 0
 -- модульная реализация
 
+p2_list :: [Int]
 p2_list = [1..1000]
 
+p2_filter :: [Int]
 p2_filter = Prelude.filter (\x -> x `mod` 3 == 0
                     || x `mod` 5 == 0) p2_list
 
+p2 :: Int
 p2 = sum p2_filter
 
 
 -- генерация последовательности
 -- при помощи отображения (map)
-
+p3_fst_l :: [Int]
 p3_fst_l = Prelude.map (*3) [1..333]
+
+p3_snd_l :: [Int]
 p3_snd_l = Prelude.map (*5) [1..200]
+
+p3_final_l :: Set Int
 p3_final_l = Set.fromList( p3_fst_l ++ p3_snd_l)
+
+p3 :: Int
 p3 = sum p3_final_l
 
 -- работа с бесконечными списками для языков поддерживающих
 -- ленивые коллекции или итераторы как часть языка
+p4_filter :: [Int]
 p4_filter = Prelude.filter (\x -> x `mod` 3 == 0
                     || x `mod` 5 == 0)
                     (takeWhile (\x -> x <= 1000) [1..])
-
+p4 :: Int
 p4 = sum p4_filter
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,40 @@
+-- module Main (main) where
+
+import Test.HUnit
+import Test.QuickCheck
+import Solution1
+import Solution30
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = runTestTTAndExit tests
+
+-- ==========================Tests 1======================================
+test1_1 :: Test
+test1_1 = TestCase (assertEqual "for (sumOfPrimes 1)," 234168 p1)
+
+test2_1 :: Test
+test2_1 = TestCase (assertEqual "for (sumOfPrimes 1)," 234168 p2)
+
+test3_1 :: Test
+test3_1 = TestCase (assertEqual "for (sumOfPrimes 1)," 234168 p3)
+
+test4_1 :: Test
+test4_1 = TestCase (assertEqual "for (sumOfPrimes 1)," 234168 p4)
+-- ==========================Tests 30=====================================
+test1_30 :: Test
+test1_30 = TestCase (assertEqual "for (sumOfPrimes 1)," 443839 sum5_1)
+
+test2_30 :: Test
+test2_30 = TestCase (assertEqual "for (sumOfPrimes 1)," 443839 sum5_2)
+
+test3_30 :: Test
+test3_30 = TestCase (assertEqual "for (sumOfPrimes 1)," 443839 sum5_3)
+
+test4_30 :: Test
+test4_30 = TestCase (assertEqual "for (sumOfPrimes 1)," 443839 sum5_4)
+
+tests :: Test
+tests = TestList [TestLabel "test1_1" test1_1, TestLabel "test2_1" test2_1,
+                  TestLabel "test3_1" test3_1, TestLabel "test3_1" test4_1,
+                  TestLabel "test1_30" test1_30, TestLabel "test2_30" test2_30,
+                  TestLabel "test3_30" test3_30, TestLabel "test4_30" test4_30]
